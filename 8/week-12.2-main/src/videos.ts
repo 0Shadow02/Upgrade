d9d357b83b22727735714a6fa48ed0d6ad8cdb16
Shadow <vidhyanshdhiman90@gmail.com>
2024-06-03 22:32:03 +0530

    8

---
+++ 8/week-12.2-main/src/videos.ts
@@ -0,0 +1,80 @@
+export interface Video {
+    title: string;
+    thumbnail: string;
+    description: string;
+    viewCount: string;
+    timestamp: string;
+}
+
+export const VIDEOS: Video[] = [
+    {
+        title: "Yuika - Sukidakara (Lyric Video)",
+        thumbnail: "/video1.jpg",
+        description: "Best Japanese songs",
+        viewCount: "1.3k",
+        timestamp: "4 hours ago"
+    },
+    {
+        title: "Yuika - Sukidakara (Lyric Video)",
+        thumbnail: "/video1.jpg",
+        description: "Best Japanese songs",
+        viewCount: "1.3k",
+        timestamp: "4 hours ago"
+    },
+    {
+        title: "Yuika - Sukidakara (Lyric Video)",
+        thumbnail: "/video1.jpg",
+        description: "Best Japanese songs",
+        viewCount: "1.3k",
+        timestamp: "4 hours ago"
+    },
+    {
+        title: "Yuika - Sukidakara (Lyric Video)",
+        thumbnail: "/video1.jpg",
+        description: "Best Japanese songs",
+        viewCount: "1.3k",
+        timestamp: "4 hours ago"
+    },
+    {
+        title: "Yuika - Sukidakara (Lyric Video)",
+        thumbnail: "/video1.jpg",
+        description: "Best Japanese songs",
+        viewCount: "1.3k",
+        timestamp: "4 hours ago"
+    },
+    {
+        title: "Yuika - Sukidakara (Lyric Video)",
+        thumbnail: "/video1.jpg",
+        description: "Best Japanese songs",
+        viewCount: "1.3k",
+        timestamp: "4 hours ago"
+    },
+    {
+        title: "Yuika - Sukidakara (Lyric Video)",
+        thumbnail: "/video1.jpg",
+        description: "Best Japanese songs",
+        viewCount: "1.3k",
+        timestamp: "4 hours ago"
+    },
+    {
+        title: "Yuika - Sukidakara (Lyric Video)",
+        thumbnail: "/video1.jpg",
+        description: "Best Japanese songs",
+        viewCount: "1.3k",
+        timestamp: "4 hours ago"
+    },
+    {
+        title: "Yuika - Sukidakara (Lyric Video)",
+        thumbnail: "/video1.jpg",
+        description: "Best Japanese songs",
+        viewCount: "1.3k",
+        timestamp: "4 hours ago"
+    },
+    {
+        title: "Yuika - Sukidakara (Lyric Video)",
+        thumbnail: "/video1.jpg",
+        description: "Best Japanese songs",
+        viewCount: "1.3k",
+        timestamp: "4 hours ago"
+    }
+]
