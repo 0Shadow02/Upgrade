4591daeb3e18ed9089f755006f3b39fb0bc5f1e2
Shadow <dark35854@gmail.com>
2024-06-10 20:25:05 +0530

    enum

--- 9/typescript_2/index.ts
+++ 9/typescript_2/index.ts
@@ -1,21 +1,29 @@
-enum ResponseStatus {
-    Success = 200,
-    NotFound = 404,
-    Error = 500
+function getFirstElement<T>(arr: T[]) {
+    return arr[0];
 }
 
-app.get("/', (req, res) => {
-    if (!req.query.userId) {
-			res.status(ResponseStatus.Error).json({})
-    }
-    // and so on...
-		res.status(ResponseStatus.Success).json({});
-})
+const el = getFirstElement(["harkiratSingh", "ramanSingh"]);
+console.log(el.toLowerCase())
+
+
+// enum ResponseStatus {
+//     Success = 200,
+//     NotFound = 404,
+//     Error = 500
+// }
+
+// app.get("/', (req, res) => {
+//     if (!req.query.userId) {
+// 			res.status(ResponseStatus.Error).json({})
+//     }
+//     // and so on...
+// 		res.status(ResponseStatus.Success).json({});
+// })
 
 
 // enum Direction {
 //     Up = "UP",
-//     Down = "Down",
+//     Down = "Down",\\\
 //     Left = "Left",
 //     Right = 'Right'
 // }
