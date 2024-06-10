3e7ccee0cc52139ff10989d49b685c24eea8e6e1
Shadow <dark35854@gmail.com>
2024-06-10 20:23:16 +0530

    enum

---
+++ 9/typescript_2/index.ts
@@ -0,0 +1,12 @@
+enum Direction {
+    Up = "UP",
+    Down = "Down",
+    Left = "Left",
+    Right = 'Right'
+}
+
+function doSomething(keyPressed: Direction) {
+	// do something.
+}
+
+doSomething(Direction.Down)
