d76b3639324f18822c8919150a1616bbc10dd0a8
Shadow <dark35854@gmail.com>
2024-06-10 20:26:25 +0530

    Generics

--- 9/typescript_2/index.ts
+++ 9/typescript_2/index.ts
@@ -1,10 +1,17 @@
-export function add(x: number, y: number): number {
-    return x + y;
+export default class Calculator {
+    add(x: number, y: number): number {
+        return x + y;
+    }
 }
 
-export function subtract(x: number, y: number): number {
-    return x - y;
-}
+
+// export function add(x: number, y: number): number {
+//     return x + y;
+// }
+
+// export function subtract(x: number, y: number): number {
+//     return x - y;
+// }
 
 
 // function getFirstElement<T>(arr: T[]) {
