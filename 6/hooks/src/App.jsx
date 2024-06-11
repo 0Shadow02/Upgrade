dd0ae8b1337e89e19f4dd2b40ebb199dccb93183
Shadow <dark35854@gmail.com>
2024-06-11 19:23:29 +0530

    ..

---
+++ 6/hooks/src/App.jsx
@@ -0,0 +1,44 @@
+import React, { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+//   const [ExchangeData, setExchangeData]= useState(200)
+ 
+//   const def = useRef()
+//   // useEffect(()=>{
+     
+//   //   setExchangeData({
+      
+//   //     returns:100
+//   //   })
+
+//   // },[])
+ 
+    
+
+//   useEffect(()=>{
+    
+//     setTimeout(() => {
+//       console.log(def.current.innerHTML)
+//       def.current.innerHTML=100
+  
+//   }, 2000);
+
+//   },[])
+ 
+ 
+
+//   return (
+//     <>
+//     {/* <h2>Your ExchangeData is {ExchangeData.returns}</h2>
+//     <h2>Your ExchangeData2 is {ExchangeData2.returns}</h2> */}
+    
+//     <div > Your text return are <h2  ref={def}>{ExchangeData} </h2> </div>
+  
+//     </>
+//   )
+// }
+}
+export default App
