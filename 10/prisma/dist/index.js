284f45b6d2a66d33340110562e1a5ad9e2fb413a
Shadow <dark35854@gmail.com>
2024-06-16 13:15:11 +0530

    prisma

---
+++ 10/prisma/dist/index.js
@@ -0,0 +1,38 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+function updateUser(username_1, _a) {
+    return __awaiter(this, arguments, void 0, function* (username, { firstName, lastName }) {
+        const res = yield prisma.user.update({
+            where: { email: username },
+            data: {
+                firstName,
+                lastName
+            }
+        });
+        console.log(res);
+    });
+}
+updateUser("shadow@gmail.com", { firstName: "dragon", lastName: "sama" });
+// async function insertUser(email:string,password:string,firstName:string , lastName:string){
+//    const res = await prisma.user.create({
+//         data:{
+//             email,
+//             password,
+//             firstName,
+//             lastName
+//         }
+//     })
+//     console.log(res)
+// }
+// insertUser("shadow@gmail.com","password@12","shadow","san")
--- 10/prisma/src/index.ts
+++ 10/prisma/src/index.ts
@@ -1,15 +1,35 @@
 import { PrismaClient } from '@prisma/client'
+import { emit } from 'process'
 const prisma = new PrismaClient()
 
-
-async function intertUser(email:string,password:string,firstName:string , lastName:string){
-   const res = await prisma.user.create({
+interface UpdateParams {
+    firstName:string,
+    lastName:string
+}
+async function updateUser(username:string, {
+    firstName,
+    lastName
+}:UpdateParams){
+   const res = await prisma.user.update({
+        where: {email:username},
         data:{
-            email,
-            password,
             firstName,
             lastName
         }
     })
     console.log(res)
 }
+updateUser("shadow@gmail.com",{firstName:"dragon",lastName:"sama"})
+// async function insertUser(email:string,password:string,firstName:string , lastName:string){
+//    const res = await prisma.user.create({
+//         data:{
+//             email,
+//             password,
+//             firstName,
+//             lastName
+//         }
+//     })
+//     console.log(res)
+// }
+// insertUser("shadow@gmail.com","password@12","shadow","san")
+
