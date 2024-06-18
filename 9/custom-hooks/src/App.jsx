34e957f24b8b1e89ac6503b722d8b1e954cbe7c1
Shadow <dark35854@gmail.com>
2024-06-18 19:14:00 +0530

    custom-hooks

--- 9/custom-hooks/src/App.jsx
+++ 9/custom-hooks/src/App.jsx
@@ -2,9 +2,33 @@
 import './App.css'
 import { useEffect, useState } from 'react';
 
-const UseIsonline =()=>{
-    window.addEventListener(window.navigator.onLine)
-
+const useIsonline =()=>{
+  const [online,setonline]= useState(window.navigator.onLine)
+    useEffect(()=>{
+      window.addEventListener('online',()=>{ setonline(true)})
+      window.addEventListener('offline',()=>{setonline(false)})
+    },[])
+   return online
+}
+const SearchBar=()=>{
+  const isonline= useIsonline()
+  console.log(isonline)
+  return<>
+  {isonline? <OnButton/>:"you are offline"}
+  </>
+}
+function OnButton(){
+  return <div>
+    <button className="relative inline-flex items-center px-6 py-3 overflow-hidden font-medium text-cyan-500 border-2 border-cyan-500 rounded-full shadow-md group">
+      <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-cyan-500 group-hover:translate-x-0 ease">
+        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+        </svg>
+      </span>
+      <span className="absolute flex items-center justify-center w-full h-full text-cyan-500 transition-all duration-300 transform group-hover:translate-x-full ease">Button</span>
+      <span className="relative invisible">Button</span>
+    </button>
+  </div>
 }
 // const SearchBar=()=>{
 //   const [time,settime]= useState(0)
