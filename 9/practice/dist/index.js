7976e9edfe7fdfcb4573a5a92494409886467911
Shadow <dark35854@gmail.com>
2024-06-23 19:05:00 +0530

    typescript

---
+++ 9/practice/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function greeting(props) {
+    console.log("HELLO " + props);
+}
+greeting("shadow");
+// let num = 3
+// num = 4
+// console.log(num)
---
+++ 9/practice/src/index.ts
@@ -0,0 +1,8 @@
+function greeting(props:string){
+console.log("HELLO " + props)
+}
+greeting("shadow")
+
+// let num = 3
+// num = 4
+// console.log(num)
