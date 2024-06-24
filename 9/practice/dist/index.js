c5ee2752e64e80dde5738b4ef591fa33099bbab8
Shadow <dark35854@gmail.com>
2024-06-24 14:05:06 +0530

    typescript

--- 9/practice/dist/index.js
+++ 9/practice/dist/index.js
@@ -1,14 +1,12 @@
 "use strict";
-function a() {
-    return 3;
-}
-function b() {
-    return 4;
-}
-function sum(a, b) {
-    console.log(a + b);
-}
-sum(a(), b());
+// const greet = (name) => `Hello, ${name}!`;
+Object.defineProperty(exports, "__esModule", { value: true });
+const num = 34;
+console.log(num);
+// function sum(a:number,b:number):number{
+//    return a + b
+// }
+// console.log(sum(10,5))
 // function greeting(props:string){
 // console.log("HELLO " + props)
 // }
--- 9/practice/src/index.ts
+++ 9/practice/src/index.ts
@@ -1,8 +1,81 @@
+// const greet = (name) => `Hello, ${name}!`;
 
-function sum(a:number,b:number){
-    console.log(a+b)
-}
+import { types } from "pg";
+import { number, string } from "zod";
 
+// function time(fn:()=>void,t:number){
+//     setTimeout(fn,t)
+// }
+
+// time(()=>{
+//     console.log("hello")
+// },3000)
+
+// function isLegal(age: number) {
+//     if (age > 18) {
+//         return true;
+//     } else {
+//         return false
+//     }
+// }
+// interface user {
+//     firstName: string,
+//     lastName: string,
+//     password: string,
+//     id: number,
+//     age:number
+// }
+
+// const User = {
+//     firstName:"shadow",
+//     lastName: "sama",
+//     password: "32kfjd",
+//     id: 234234,
+//     age:23
+// }
+// // console.log(User)
+// function isLegal(user:user){
+//     if(user.age <18){
+//         return "User is under 18"
+//     }
+//     else{
+//         return "User is over 18"
+//     }
+// }
+
+// console.log(isLegal(User))
+
+
+// type User ={
+//     firstName:string
+//     lastName: string
+//     age: number
+// }
+
+type stringornumber = string|number
+const num:stringornumber= 34
+console.log(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sum(a:number,b:number):number{
+//    return a + b
+// }
+// console.log(sum(10,5))
 
 // function greeting(props:string){
 // console.log("HELLO " + props)
@@ -12,3 +85,25 @@ function sum(a:number,b:number){
 // let num = 3
 // num = 4
 // console.log(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
