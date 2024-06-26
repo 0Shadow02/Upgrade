f9368b0bf7a84ec13acc78d72dc9550efa83deb8
Shadow <dark35854@gmail.com>
2024-06-26 16:03:25 +0530

    typescript

--- 11/cloudflare-workers/src/index.ts
+++ 11/cloudflare-workers/src/index.ts
@@ -1,8 +1,14 @@
 import { Hono } from 'hono'
+
 const app = new Hono()
 
-app.get('/',(c)=> {
-	return c.json("hello")
+app.get('/', async (c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.text('Hello Hono!')
 })
 
 export default app
