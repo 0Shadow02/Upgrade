20baf510dbfe6e1c6357cbc9b185b521694d8c16
Shadow <dark35854@gmail.com>
2024-06-26 16:51:05 +0530

    typescript

--- 11/cloudflare-workers/src/index.ts
+++ 11/cloudflare-workers/src/index.ts
@@ -2,7 +2,17 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', async (c) => {
+app.use(async (c, next) => {
+  if (c.req.header("Authorization")) {
+    // Do validation
+    await next()
+  } else {
+    return c.text("You dont have acces");
+  }
+})
+
+
+app.post('/', async (c) => {
   const body = await c.req.json()
   console.log(body);
   console.log(c.req.header("Authorization"));
