3e26f7f9a9f5cd1522e13088bd542cd9677afe48
Shadow <dark35854@gmail.com>
2024-07-17 12:52:58 +0530

    Data_Fething_Next

---
+++ 13/next_backend/src/app/page.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import Image from "next/image";
+
+
+async function getUserDetails(){
+  await new Promise(r=>setTimeout(r,2000))
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  return response.data
+
+}
+
+
+export default async function Home() {
+  const userData = await getUserDetails()
+
+  return (  <div className="flex flex-col justify-center h-screen">
+    <div className="flex justify-center">
+        <div className="border p-8 rounded">
+            <div>
+                Name: {userData?.name}
+            </div>
+            
+            {userData?.email}
+        </div>
+    </div>
+</div>
+  );
+}
