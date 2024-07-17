93cd61ce16a4f12dae212380ff6794af3638fddc
Shadow <dark35854@gmail.com>
2024-07-17 13:15:36 +0530

    Data_Fething_Next

---
+++ 13/next_backend/src/app/api/user/route.ts
@@ -0,0 +1,7 @@
+
+export function GET(){
+    return Response.json({
+        email:"shadow@gmailcom",
+        name : "shadow"
+    })
+}
--- 13/next_backend/src/app/page.tsx
+++ 13/next_backend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 
 async function getUserDetails(){
-  await new Promise(r=>setTimeout(r,2000))
+
   const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
   return response.data
 
