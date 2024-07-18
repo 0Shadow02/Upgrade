4c190ccdb0acb4938e0f9e664088b556e7d33f1c
Shadow <dark35854@gmail.com>
2024-07-18 18:36:42 +0530

    Data_Fething_Next

--- 13/practic_next/components/Button.tsx
+++ 13/practic_next/components/Button.tsx
@@ -10,7 +10,7 @@ export function Button({username,password}:userdata){
      
     
     return <div>
-        <button onClick={()=>{}} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Login</button>
+        <button onClick={handler} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Login</button>
     </div>
 }
 
