a0104919c3bd7b1bbda31edd2a29a599def033ae
Shadow <dark35854@gmail.com>
2024-07-18 18:37:01 +0530

    Data_Fething_Next

--- 13/practic_next/components/Button.tsx
+++ 13/practic_next/components/Button.tsx
@@ -8,7 +8,13 @@ interface userdata{
 }
 export function Button({username,password}:userdata){
      
-    
+     async function handler(){
+          axios.post("http://localhost:3000/api/user", {
+            username,
+            password
+        }).then(()=>console.log(username) )
+        
+     }
     return <div>
         <button onClick={handler} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Login</button>
     </div>
