dabd9870ecba5d3f76d2a77a831939b99ab143a2
Shadow <dark35854@gmail.com>
2024-07-18 18:35:31 +0530

    Data_Fething_Next

--- 13/practic_next/app/api/user/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from "next/server"
-
-export async function GET(){
-    return Response.json({name:"shadow",email:"shadow@gmail.com"})
-}
-export async function POST(req:NextRequest){
-     const body = await req.json()
-    return Response.json({msg:"done",body:body})
-}
