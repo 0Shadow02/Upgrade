f2502d37d5ac58df0e5a5b7f65fd863491b8d27f
Shadow <dark35854@gmail.com>
2024-08-15 17:00:04 +0530

    using-express

--- 19/websockets/src/index.ts
+++ 19/websockets/src/index.ts
@@ -1,12 +1,37 @@
-import WebSocket, { WebSocketServer } from 'ws';
-import http from 'http';
+// import WebSocket, { WebSocketServer } from 'ws';
+// import http from 'http';
 
-const server = http.createServer(function(request: any, response: any) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.end("hi there");
-});
+// const server = http.createServer(function(request: any, response: any) {
+//     console.log((new Date()) + ' Received request for ' + request.url);
+//     response.end("hi there new code");
+// });
 
-const wss = new WebSocketServer({ server });
+// const wss = new WebSocketServer({ server });
+
+// wss.on('connection', function connection(ws) {
+//   ws.on('error', console.error);
+
+//   ws.on('message', function message(data, isBinary) {
+//     wss.clients.forEach(function each(client) {
+//       if (client.readyState === WebSocket.OPEN) {
+//         client.send(data, { binary: isBinary });
+//       }
+//     });
+//   });
+
+//   ws.send('Hello! Message From Server!!');
+// });
+
+// server.listen(8080, function() {
+//     console.log((new Date()) + ' Server is listening on port 8080');
+// });
+import express from 'express'
+import { WebSocketServer } from 'ws'
+
+const app = express()
+const httpServer = app.listen(8080)
+
+const wss = new WebSocketServer({ server: httpServer });
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
@@ -20,8 +45,4 @@ wss.on('connection', function connection(ws) {
   });
 
   ws.send('Hello! Message From Server!!');
-});
-
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
 });
