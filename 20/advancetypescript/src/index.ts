09cd75130ed1277e982b61927dfef1fb393bc8b4
Shadow <dark35854@gmail.com>
2024-08-27 15:58:24 +0530

    advance_ts

--- 20/advancetypescript/src/index.ts
+++ 20/advancetypescript/src/index.ts
@@ -23,11 +23,11 @@
 //     name: string;
 //   }
 
-//   type Users = { [key:string] :User }
-//   const users: Users = {
-//     'abc123': { id: 'abc123', name: 'John Doe' },
-//     'xyz789': { id: 'xyz789', name: 'Jane Doe' },
-//   };
+  type Users = { [key:string] :User }
+  const users: Users = {
+    'abc123': { id: 'abc123', name: 'John Doe' },
+    'xyz789': { id: 'xyz789', name: 'Jane Doe' },
+  };
 import { z } from 'zod';
 import express from "express";
 
