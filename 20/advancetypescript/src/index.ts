c377b7417535103dc025f57f124ade3c090a8fc3
Shadow <dark35854@gmail.com>
2024-08-27 16:02:43 +0530

    advance_ts

--- 20/advancetypescript/src/index.ts
+++ 20/advancetypescript/src/index.ts
@@ -1,14 +1,14 @@
-// interface User {
-// 	name: string;
-// 	age: number;
-// }
-// type updatedprop = Pick<User,"age" >
+interface User {
+	name: string;
+	age: number;
+}
+type updatedprop = Pick<User,"age" >
 
-// function sumOfAge(user1: User, user2: updatedprop) {
-//   return user1.age + user2.age;
-// };
+function sumOfAge(user1: User, user2: updatedprop) {
+  return user1.age + user2.age;
+};
 
-// // Example usage
+// Example usage
 // const result = sumOfAge({
 // 	name: "harkirat",
 // 	age: 20
@@ -24,10 +24,10 @@ interface User {
   }
 
   type Users = { [key:string] :User }
-  const users: Users = {
-    'abc123': { id: 'abc123', name: 'John Doe' },
-    'xyz789': { id: 'xyz789', name: 'Jane Doe' },
-  };
+  // const users: Users = {
+  //   'abc123': { id: 'abc123', name: 'John Doe' },
+  //   'xyz789': { id: 'xyz789', name: 'Jane Doe' },
+  // };
 import { z } from 'zod';
 import express from "express";
 
