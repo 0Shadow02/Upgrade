4c8179ab0594f8c40ace9e75ad7dc091b9a818ad
Shadow <dark35854@gmail.com>
2024-08-31 09:59:50 +0530

    Eslint

--- 21/express-eslint/eslint.config.mjs
+++ 21/express-eslint/eslint.config.mjs
@@ -2,10 +2,18 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  {
+    languageOptions: { globals: globals.node },
+    rules: {
+      // Indentation rule: 2 spaces
+      "indent": ["error", 2],
+      // Space before function parenthesis
+      "space-before-function-paren": ["error", "always"],
+      // No space inside parentheses
+      "space-in-parens": ["error", "never"]
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
--- 21/express-eslint/src/index.ts
+++ 21/express-eslint/src/index.ts
@@ -1,10 +1,9 @@
 import express from "express";
 
 const app = express();
-let x = 1;
 
 app.get("/", (req, res) => {
-	res.json({
-		message: "Hi there"
-	});
+  res.json({
+    message: "Hi there"
+  });
 });
